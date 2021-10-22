804fba98be710558bedcdaa0fd60e07970de69eb
Momolu <lenny0012@hotmail.com>
2021-10-22 14:24:32 -0500

    Semana 1 completo

--- semana1/challenge1.js
+++ semana1/challenge1.js
@@ -1,5 +1,14 @@
 const reverseString = (str) => {
     //Implementación
+    if(str.length > 1 && str.length < 15){
+        let result = '';
+        for (let i = str.length-1; i >= 0; i--) {
+            const element = str[i];
+            result += element;
+        }
+        return result;
+    }
+    throw new Error("argumento novalido");
 }
 
 module.exports = {
--- semana1/challenge2.js
+++ semana1/challenge2.js
@@ -1,5 +1,15 @@
 const reverseInt = (number) => {
     //Implementación
+    if(typeof(number) === 'number')
+    {
+        let tostring = number.toString().split('').reverse().join('');
+        if( tostring[tostring.length-1] === '-') {
+            return tostring = parseInt('-'+tostring);
+        }else
+        return tostring = parseInt(tostring);
+        
+    }
+    throw new Error ("Argumento no valido");
 }
 
 module.exports = {
